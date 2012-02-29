f8137cc04d6d80d62c2a5498c75091eeef90cddd
Yen Ha Thi Bach <yencn02@gmail.com>
2012-02-29 10:18:34 +0700

    Fix bug login facebook

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Hapful::Application.initialize!
+
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
